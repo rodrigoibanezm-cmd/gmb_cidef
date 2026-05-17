e6fd053d4e57b70abc40553af9b43ae68fafd7e5
rodrigoibanezm-cmd <rodrigo.ibanezm@gmail.com>
2026-05-17 12:51:09 -0400

    Remove unused light-step endpoint

--- api/gmb/update/light-step.js
+++
@@ -1,7 +0,0 @@
-import lightHandler from "./light.js";
-
-export default async function handler(req, res) {
-  req.query.limit = req.query.limit || "25";
-  req.query.max_batches = req.query.max_batches || "2";
-  return lightHandler(req, res);
-}
